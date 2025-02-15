7bb6783685b8bd15387fd2f15cb2b7c9ff9f9947
John Doe <johndoe@email.com>
2025-02-15 20:49:39 +0000

    style: navbar customization

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,14 @@
-import Navbar from "@/components/general/Navbar";
+import { Navbar } from "@/components/general/Navbar";
 
-export default function MainLayout( {children} : {children: React.ReactNode}) {
-    return (
-        <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
-            <Navbar />
-            {children}
-        </div>
-    )
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
+      <Navbar />
+      {children}
+    </div>
+  );
 }
