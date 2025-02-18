bc9162d841dc9ae543718a48ca00660d53e10961
John Doe <johndoe@email.com>
2025-02-18 04:36:24 +0000

    feat: ui for homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,13 @@
+import { JobFilters } from "@/components/general/JobFilters";
+import { JobListings } from "@/components/general/JobListings";
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="grid grid-cols-3 gap-8">
+      <JobFilters />
+      <div className="col-span-2 flex flex-col gap-6">
+      <JobListings />
+      </div>
+    </div>
+  );
 }
